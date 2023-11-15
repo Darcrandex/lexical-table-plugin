84a622fe179823342552ef3744c30eef35573621
darcrand <darcrandex@gmail.com>
2023-11-15 17:31:55 +0800

    ✨ feat(table): 删除

--- src/components/DocEditor/plugins/FormTable/TopCellMenus/TableTools.tsx
+++ src/components/DocEditor/plugins/FormTable/TopCellMenus/TableTools.tsx
@@ -4,14 +4,34 @@
  * @author darcrand
  */
 
-import { NodeKey } from 'lexical'
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext'
+import { $getNodeByKey, NodeKey } from 'lexical'
+import { useCallback } from 'react'
+import { $isFormTableNode } from '../FormTableNode'
 
 export type TableToolsProps = { className?: string }
 
 export default function TableTools(props: TableToolsProps & { nodeKey: NodeKey }) {
+  const [editor] = useLexicalComposerContext()
+
+  const onRemove = useCallback(() => {
+    if (props.nodeKey) {
+      editor.update(() => {
+        const node = $getNodeByKey(props.nodeKey)
+        if ($isFormTableNode(node)) {
+          node.remove()
+        }
+      })
+    }
+
+    return false
+  }, [editor, props.nodeKey])
+
   return (
     <>
-      <button>删除表格</button>
+      <button className='disabled:text-gray-400 disabled:cursor-not-allowed' onClick={() => onRemove()}>
+        删除表格
+      </button>
     </>
   )
 }
